7ede569293d19d8ebdc5ff01f2312ee2239f7668
Karol Makara <karol_makara17@wp.pl>
2019-04-15 22:51:29 +0200

    Updates

---
+++ precache-manifest.5cd1e6f5201b05f0cba548bc14efa3a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b2c9ec348ee56ff6581",
+    "url": "/ShoppingList/static/js/runtime~main.4013c62d.js"
+  },
+  {
+    "revision": "43ff0f48d2cd87802837",
+    "url": "/ShoppingList/static/js/main.8abdf8c2.chunk.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ShoppingList/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "43ff0f48d2cd87802837",
+    "url": "/ShoppingList/static/css/main.57a3b85e.chunk.css"
+  },
+  {
+    "revision": "006d08b5468d884742d9c2f67b9bf2b6",
+    "url": "/ShoppingList/index.html"
+  }
+];
